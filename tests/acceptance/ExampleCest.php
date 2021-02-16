eb4df505186f99710708c2892b8d78550902e554
Ryan Meier <ryan.meier@wpengine.com>
2021-02-16 09:35:28 -0600

    Updated to use Docker, WebDriver, and Chromedriver

--- tests/acceptance/ExampleCest.php
+++ tests/acceptance/ExampleCest.php
@@ -2,6 +2,25 @@
 
 class ExampleCest
 {
+    /**
+     * @type string
+     */
+    protected $wordPressUrl;
+
+    /**
+     * Save the WordPress url for later use.
+     */
+    public function _before(AcceptanceTester $I)
+    {
+        $this->wordPressUrl = getenv('TEST_SITE_WP_URL');
+    }
+
+    /**
+     * A simple test.
+     *
+     * 1. Test Codeception can search the WordPress database
+     * 2. Test Codeception can see 'Hello world' on the home page.
+     */
     public function i_see_hello_world(AcceptanceTester $I)
     {
         $I->havePostInDatabase([
@@ -13,15 +32,23 @@ class ExampleCest
         $I->see('Hello world!');
     }
 
+    /**
+     * Test domain switching.
+     *
+     * 1. Test browser can see the plugin title on the plugins page.
+     * 2. Test browser can switch domains and see some text.
+     * 3. Test browser can switch back to WordPress and see the plugin title on the plugins page.
+     */
     public function i_can_switch_between_domains(AcceptanceTester $I)
     {
         $I->loginAsAdmin();
         $I->amOnPluginsPage();
         $I->see('Codeception WordPress Example');
 
-        $I->amOnPage('https://duckduckgo.com/');
-        $I->see('Privacy, simplified.');
+        $I->amOnUrl('https://duckduckgo.com/');
+        $I->see('Tired of being tracked online?');
 
+        $I->amOnUrl($this->wordPressUrl);
         $I->amOnPluginsPage();
         $I->see('Codeception WordPress Example');
     }
